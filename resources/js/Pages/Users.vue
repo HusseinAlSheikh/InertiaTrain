d19f3f9815b66d338f904bc665241522306bfb2e
Hussein Alsheikh <hussein.jodd@gmail.com>
2024-04-22 12:39:25 +0300

    first commit

---
+++ resources/js/Pages/Users.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1> Hello Users </h1>
+    <Nav/>
+</template>
+
+
+<script>
+
+    import Nav from '../Shared/Nav.vue';
+    export default {
+        components : {Nav}
+        
+    };
+</script>
